59ef50ff46ab8853315341f1830d2f04982eea91
Samuel-Kepler <sr123456123@gmail.com>
2024-03-28 18:03:18 -0300

    feat: Adicionando pagina inical

--- js/script.js
+++ js/script.js
@@ -1,12 +1,6 @@
-$(function(){
-    $("#navbar").load("../navbar/navbar.html"); 
-  });
-
 $(function() {
-    $('#botao-mobile').on('click', function () {
-       $('#menu-mobile').toggleClass("active");
-       $('#menu-mobile').find("i").toggleClass("fa-x");
-    })
+  $('#botao-mobile').on('click', function () {
+      $('#menu-mobile').toggleClass("active");
+      $('#botao-mobile').find("i").toggleClass("fa-solid fa-x");
+  })
 });
-
-
