42fc96c18a5821a244fa8324bffca611317573d9
Samuel-Kepler <sr123456123@gmail.com>
2024-04-09 17:51:00 -0300

    feat: Adicionando a seção causas

--- js/script.js
+++ js/script.js
@@ -4,3 +4,52 @@ $(function() {
       $('#botao-mobile').find("i").toggleClass("fa-solid fa-x");
   })
 });
+
+$(document).ready(function() {
+  let slides = $(".carousel-item");
+  let dots = $(".carousel-indicator");
+
+  let index = 1;
+  showSlides(index);
+
+  $('.voltar').click(function(event) {
+      event.preventDefault();
+      addSlides(-1);
+  });
+
+  $('.next-button').click(function() {
+      addSlides(1);
+  });
+
+  dots.each(function(i) {
+      $(this).click(function() {
+          currentSlide(i + 1);
+      });
+  });
+
+  function addSlides(n) {
+      showSlides(index += n);
+  }
+
+  function currentSlide(n) {
+      showSlides(index = n);
+  }
+
+  function showSlides(n) {
+      if (n > slides.length) {
+          index = 1;
+      }
+
+      if (n < 1) {
+          index = slides.length;
+      }
+
+      slides.each(function(i) {
+          $(this).css('display', 'none');
+          $(dots[i]).removeClass("actived");
+      });
+
+      $(slides[index - 1]).css('display', 'flex');
+      $(dots[index - 1]).toggleClass("actived");
+  }
+});
