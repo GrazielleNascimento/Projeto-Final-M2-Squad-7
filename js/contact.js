caefd9330098857f1de0ee251819e83b6c70829e
Samuel-Kepler <sr123456123@gmail.com>
2024-04-11 14:00:57 -0300

    refactor: Corrigindo redirecionamento da pagina contato e obrigado

---
+++ js/contact.js
@@ -0,0 +1,5 @@
+
+document.getElementById('myForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    window.location.href = 'http://127.0.0.1:5500/paginas/pagina-obrigado.html';
+});
